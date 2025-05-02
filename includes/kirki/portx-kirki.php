2926011a61e9548e5e2ea15897910e0ffac132b7
tamimhasan19702 <tamimhasancu@gmail.com>
2025-05-02 19:56:35 +0600

    update portx-kirki.php and header-1.php

--- includes/kirki/portx-kirki.php
+++ includes/kirki/portx-kirki.php
@@ -33,6 +33,40 @@ function portx_header_section()
                 'section' => 'portx_header_section',
                 'default' => get_template_directory_uri() . '/assets/img/logo/logo-white.png',
             ]);
+            \Kirki::add_field('portx_contact_info', [
+                'type' => 'repeater',
+                'settings' => 'portx_contact_info',
+                'label' => esc_html__('Portx Contact Info', 'portx'),
+                'description' => esc_html__('You can add multiple contact blocks here.', 'portx'),
+                'section' => 'portx_header_section',
+                'priority' => 10,
+                'row_label' => [
+                    'type' => 'text',
+                    'value' => esc_attr__('Contact Info', 'portx'),
+                ],
+                'button_label' => esc_html__('Add New Contact Info', 'portx'),
+                'fields' => [
+                    'flaticon_class' => [
+                        'type' => 'text',
+                        'label' => esc_html__('Flaticon Class', 'portx'),
+                        'description' => esc_html__('Add the flaticon class name here.', 'portx'),
+                    ],
+                    'header' => [
+                        'type' => 'text',
+                        'label' => esc_html__('Header', 'portx'),
+                    ],
+                    'description' => [
+                        'type' => 'textarea',
+                        'label' => esc_html__('Description', 'portx'),
+                    ],
+                    'url' => [
+                        'type' => 'url',
+                        'label' => esc_html__('Url', 'portx'),
+                        'description' => esc_html__('The saved value will be the URL.', 'portx'),
+                    ],
+                ],
+            ]);
+
 
         }
     }
---
+++ template-parts/header/tpoffcanvas-area.php
@@ -0,0 +1,28 @@
+<div class="tpoffcanvas-area">
+    <div class="tpoffcanvas">
+        <div class="tpoffcanvas__close-btn">
+            <button class="close-btn"><i class="fal fa-times"></i></button>
+        </div>
+        <div class="tpoffcanvas__logo">
+            <a href="index.html">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/logo/footer-logo.png'); ?>"
+                    alt="">
+            </a>
+        </div>
+        <div class="tp-main-menu-mobile"></div>
+        <div class="offcanvas__btn mb-20">
+            <a href="contact.html" class="tp-btn w-100">Getting Started</a>
+        </div>
+        <div class="offcanvas__contact mb-40">
+            <p class="offcanvas__contact-call"><a href="tel:+964-742-44-763">+964 742 44 763</a></p>
+            <p class="offcanvas__contact-mail"><a href="mailto:info@harry.com">info@seomy.com</a></p>
+        </div>
+        <div class="offcanvas__social">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+</div>
+<div class="body-overlay"></div>
